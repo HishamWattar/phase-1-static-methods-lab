6881f3cba01b432a3ad7d37d3d3b07177926bbf6
hisham <h.wattar@netro.xyz>
2023-04-12 21:24:45 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z '-]/g, '')
+    }
+
+    static titleize(string) {
+        const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+        const words = string.split(' ')
+        return words.map(function (item) {
+            if (!exceptions.includes(item) || words.indexOf(item) === 0) {
+                return item.charAt(0).toUpperCase() + item.slice(1)
+            }
+            return item
+        }).join(' ')
+    }
 }
